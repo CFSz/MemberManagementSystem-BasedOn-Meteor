8754f9efb9b3c06ef64be5242ca3cc0532eb38ab
CFSz <781389087@qq.com>
2015-12-18 15:15:27 +0800

    callbacks的配置比预想中复杂,暂且搁置开发

--- client/js/upload.js
+++ client/js/upload.js
@@ -1,8 +1,12 @@
 Template.input.helpers({
     uploadCallback: function () {
         return {
-            validate: function (file) {
-                console.log(file);
+            //validate: function (file) {
+            //    console.log(file);
+            //    return true;
+            //}
+            finished: function (index, fileInfo, context) {
+                console.log(arguments);
             }
         }
     }
